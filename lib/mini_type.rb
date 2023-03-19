2856fc35b5f170f5c0f7f8d555c87a4baf8fabac
Aaron Gough <aaron.gough@testdouble.com>
2023-03-19 16:35:29 -0400

    Remove use of PARAMETER in constant name, change to ARGUMENT

--- lib/mini_type.rb
+++ lib/mini_type.rb
@@ -7,7 +7,7 @@ module MiniType
 
   class IncorrectArgumentType < MiniTypeError; end
 
-  INCORRECT_PARAMETER_TYPE = "Expected argument ':%s' to be of type '%s', but got '%s'"
+  INCORRECT_ARGUMENT_TYPE = "Expected argument ':%s' to be of type '%s', but got '%s'"
   INVALID_DECLARAION = "Invalid type declaration. The `accepts` method expects a block that returns a hash, like: `accepts {{ foo: String }}`. Received: '%s'"
   INVALID_TYPE = "Invalid type declaration for: ':%s'. Type declaration should be a class literal (`String`), an aray of class literals (`[String, NilClass]`), or a matcher (`array_of(String)`)."
 
@@ -40,9 +40,9 @@ module MiniType
         if allowable_type.respond_to?(:call)
           allowable_type.call(argument_name, argument_value)
         elsif allowable_type.is_a?(Array)
-          raise(IncorrectArgumentType, INCORRECT_PARAMETER_TYPE % [argument_name, allowable_type, argument_value.class.name]) unless allowable_type.include?(argument_value.class)
+          raise(IncorrectArgumentType, INCORRECT_ARGUMENT_TYPE % [argument_name, allowable_type, argument_value.class.name]) unless allowable_type.include?(argument_value.class)
         elsif allowable_type.is_a?(Class)
-          raise(IncorrectArgumentType, INCORRECT_PARAMETER_TYPE % [argument_name, allowable_type, argument_value.class.name]) unless argument_value.is_a?(allowable_type)
+          raise(IncorrectArgumentType, INCORRECT_ARGUMENT_TYPE % [argument_name, allowable_type, argument_value.class.name]) unless argument_value.is_a?(allowable_type)
         else
           raise(IncorrectArgumentType, INVALID_TYPE % [argument_name])
         end
